9fd0fcf4ddb4fe90b3da139d992838cee6e7ac01
bianca <biancaa.bobescu@gmail.com>
2020-02-05 20:13:40 +0100

    Current state

--- test_suites/features/step_definitions/phptravels.rb
+++ test_suites/features/step_definitions/phptravels.rb
@@ -1,3 +1,5 @@
+require 'date'
+$parsedResults = []
 Given("user navigates to homepage") do
   @browser.goto $homepage
 end
@@ -31,8 +33,68 @@ Given("user searches following flights") do |table|
   toMatch.fire_event("mouseup")
 
   #departure date
-  calendar = @browser.input(id: "FlightsDateStart")
+  # open datepicker
+  calendar = @browser.text_field(id: "FlightsDateStart")
   calendar.fire_event("focus")
   calendar.fire_event("mouseup")
 
+  #what on earth? :D
+  departureDate = Date.today + table.hashes[0]["departureIn"].to_i
+  inputDeparture = @browser.div(:class => %w(datepicker active))
+                       .div(:data_date => departureDate.day.to_s, :data_month => (departureDate.month() - 1).to_s, :data_year => departureDate.year.to_s)
+  inputDeparture.fire_event("mousedown")
+  inputDeparture.fire_event("click")
+
+  #set adults
+  addAdultButton = @browser.element(:xpath => "//*[@id='flights']/div/div/form/div/div[3]/div[3]/div/div/div[1]/div/div[2]/div/span/button[1]")
+  nrOfAdults = table.hashes[0]["adults"].to_i() - 1
+  nrOfAdults.times do
+    addAdultButton.fire_event("mousedown")
+    addAdultButton.fire_event("mouseup")
+  end
+
+  #set children
+  addChildrenButton = @browser.element(:xpath => "//*[@id='flights']/div/div/form/div/div[3]/div[3]/div/div/div[2]/div/div[2]/div/span/button[1]")
+  nrOfChildren = table.hashes[0]["children"].to_i
+  nrOfChildren.times do
+    addChildrenButton.fire_event("mousedown")
+    addChildrenButton.fire_event("mouseup")
+  end
+
+  #search
+  searchButton = @browser.element(:xpath => "//*[@id='flights']/div/div/form/div/div[3]/div[4]/button")
+  searchButton.fire_event("mousedown")
+  searchButton.fire_event("click")
+  parseResults
+end
+
+
+Then("the displayed results should be sorted by price ascending") do
+
+  previousPrice = -1
+  $parsedResults.each do |flight|
+    assert previousPrice < flight["price"], "Prices are not sorted ascending"
+    previousPrice = flight["price"]
+  end
+end
+
+
+Then("the displayed results should have {string} as departure point and {string} as destination") do |expectedDeparturePoint, expectedDestination|
+  $parsedResults.each do |flight|
+    assert expectedDeparturePoint < flight["startLocation"], "Flight doesn't have expected startLocation"
+    assert expectedDestination < flight["startLocation"], "Flight doesn't have expected destination"
+  end
+end
+
+
+def parseResults
+  results = @browser.ul(id: "LIST").wait_until(&:present?)
+  results.each do |li|
+    price = li.element(class: "theme-search-results-item-price-tag").text.match(/(\d+)/)[1]
+    startLocation = li.element(class: "theme-search-results-item-flight-section-meta-city").text
+
+    # same principle for getting ahold of the trip durations and trip destination
+    # but page changed layout respectively uses same identifier for destination (facepalm)
+    $parsedResults.push(Hash["price" => price, "destination" => "MUC", "startLocation" => startLocation, "tripDuration" => 10])
+  end
 end
--- test_suites/features/testcases/phptravels.feature
+++ test_suites/features/testcases/phptravels.feature
@@ -1,8 +1,20 @@
 Feature: Php Travels
 
-  Scenario: User searches for flights and the results are sorted ascending by price
-
+  Background:
     Given user navigates to homepage
     Given user searches following flights
-      | from | to  | departureIn(days) | adults | children | infants |
-      | JFK  | MUC | 14                | 2      | 2        | 0       |
+      | from | to  | departureIn | adults | children |
+      | JFK  | MUC | 14          | 2      | 2        |
+
+  Scenario: Result flights are sorted ascending by price
+    Then the displayed results should be sorted by price ascending
+
+  Scenario: All result flights have NYC as departure point and MUC as destination
+    Then the displayed results should have "NYC" as departure point and "MUC" as destination
+
+  #Scenario: User chooses the shortest route and books it
+   # Given the user chooses the shortest route and proceeds with booking
+    #Given the user enters  credentials
+    #Given user fills the forms and accepts terms and conditions
+    #When user submits form
+    #Then he should receive the bill
